00ee5fb443b9394fa9942e83e24cf2c8a8f0d9e0
Stein-N <nico-stein-98@sbvl.de>
2025-11-17 18:43:29 +0100

    add basic test code

---
+++ Data/FrameData.lua
@@ -0,0 +1,59 @@
+local playerContext = PlayerFrame.PlayerFrameContent.PlayerFrameContentContextual
+local playerContainer = PlayerFrame.PlayerFrameContainer
+local playerHealth = PlayerFrame.PlayerFrameContent.PlayerFrameContentMain.HealthBarsContainer
+local playerMana = PlayerFrame.PlayerFrameContent.PlayerFrameContentMain.ManaBarArea
+
+PlayerContext = {
+    attack_icon = playerContext.AttackIcon,
+    group_indicator = playerContext.GroupIndicator,
+    guide_icon = playerContext.GuideIcon,
+    leader_icon = playerContext.LeaderIcon,
+    pvp_icon = playerContext.PVPIcon,
+    play_time = playerContext.PlayerPlayTime,
+    portrait_corner_icon = playerContext.PlayerPortraitCornerIcon,
+    rest_loop = playerContext.PlayerRestLoop,
+    prestige_portrait = playerContext.PrestigePortrait,
+    pvp_timer_text = playerContext.PvpTimerText,
+    ready_check = playerContext.ReadyCheck,
+    role_icon = playerContext.RoleIcon
+}
+
+PlayerBars = {
+    health_bar = playerHealth.HealthBar,
+    health_bar_mask = playerHealth.HealthBarMask,
+    health_bar_text = playerHealth.HealthBarText,
+    health_left_text = playerHealth.LeftText,
+    health_animated_loss = playerHealth.PlayerFrameHealthBarAnimatedLoss,
+    health_temp_max_loss = playerHealth.PlayerFrameTempMaxHealthLoss,
+    health_right_text = playerHealth.RightText,
+    health_temp_max_loss_divider = playerHealth.TempMaxHealthLossDivider,
+
+    mana_bar = playerMana.ManaBar,
+    mana_bar_mask = playerMana.ManaBar.ManaBarMask,
+    mana_bar_text = playerMana.ManaBar.ManaBarText,
+    mana_left_text = playerMana.ManaBar.LeftText,
+    mana_right_text = playerMana.ManaBar.RightText
+}
+
+PlayerContainer = {
+    portrait = playerContainer.PlayerPortrait,
+    portrait_mask = playerContainer.PlayerPortraitMask,
+    frame_flash = playerContainer.FrameFlash,
+    frame_texture = playerContainer.FrameTexture, -- Border Texture for Health/Mana bar
+    vehicle = playerContainer.VehicleFrameTexture,
+    alternative_power_bar = playerContainer.AlternatePowerFrameTexture,
+    status_texture = PlayerFrame.PlayerFrameContent.PlayerFrameContentMain.StatusTexture,
+    player_name = PlayerName,
+    player_level = PlayerLevelText
+}
+
+ResourceFrames = {
+    WARLOCK = WarlockPowerFrame,
+    ROGUE = RogueComboPointBarFrame,
+    DRUID = DruidComboPointBarFrame,
+    PALADIN = PaladinPowerBarFrame,
+    DEATHKNIGHT = RuneFrame,
+    EVOKER = EssencePlayerFrame,
+    MAGE = MageArcaneChargesFrame,
+    MONK = MonkHarmonyBarFrame
+}
--- Events/BufEvents.lua
+++ Events/BufEvents.lua
@@ -13,5 +13,45 @@ function handler.ADDON_LOADED(name)
 end
 
 function handler.PLAYER_ENTERING_WORLD()
+    if PlayerContainer.portrait then PlayerContainer.portrait:Hide() end
 
+    for _, element in pairs(PlayerContext) do element:Hide() end
+    for _, element in pairs(PlayerBars) do element:Hide() end
+    for _, element in pairs(PlayerContainer) do element:Hide() end
+
+    PlayerContainer.frame_texture:Show()
+    PlayerContainer.frame_texture:SetTexture("Interface\\AddOns\\BetterUnitFrame\\Media\\No-Portrait-2-Rows")
+
+    PlayerBars.health_bar:Show()
+    PlayerBars.health_bar:SetStatusBarTexture("UI-HUD-UnitFrame-Player-PortraitOff-Bar-Health")
+    PlayerBars.health_bar:ClearAllPoints()
+    PlayerBars.health_bar:SetPoint("TOPLEFT", PlayerFrame, "TOPLEFT", 21, -25)
+    PlayerBars.health_bar:SetWidth(190)
+    PlayerBars.health_bar:SetHeight(28)
+
+    PlayerBars.health_bar_text:ClearAllPoints()
+    PlayerBars.health_bar_text:SetPoint("CENTER", PlayerBars.health_bar, "CENTER")
+    PlayerBars.health_bar_text:SetScale(1.5)
+    PlayerBars.health_bar_text:Show()
+
+    PlayerBars.mana_bar:Show()
+    -- PlayerBars.mana_bar:SetStatusBarTexture("UI-HUD-UnitFrame-Player-PortraitOff-Bar-RunicPower")
+    PlayerBars.mana_bar:ClearAllPoints()
+    PlayerBars.mana_bar:SetPoint("TOPLEFT", PlayerFrame, "TOPLEFT", 22, -54)
+    PlayerBars.mana_bar:SetWidth(188)
+    PlayerBars.mana_bar:SetHeight(13)
+
+    if ResourceFrames.DEATHKNIGHT then
+        ResourceFrames.DEATHKNIGHT:ClearAllPoints()
+        ResourceFrames.DEATHKNIGHT:SetPoint("CENTER", PlayerFrame, "BOTTOM", 0, 0)
+    end
+
+    PlayerContainer.player_name:Show()
+    PlayerContainer.player_name:ClearAllPoints()
+    PlayerContainer.player_name:SetPoint("TOPLEFT", PlayerFrame, "TOPLEFT", 35, -14)
+
+
+    PlayerContainer.player_level:Show()
+    PlayerContainer.player_level:ClearAllPoints()
+    PlayerContainer.player_level:SetPoint("TOPRIGHT", PlayerFrame, "TOPRIGHT", -30, -14)
 end
