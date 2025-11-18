c36fe468daa205a434ae7ccd93a05aad768dff88
Stein-N <nico-stein-98@sbvl.de>
2025-11-18 16:34:45 +0100

    add more textures and test them

--- Events/BufEvents.lua
+++ Events/BufEvents.lua
@@ -22,39 +22,45 @@ function handler.PLAYER_ENTERING_WORLD()
     local _, class = UnitClass("player")
     local classColor = RAID_CLASS_COLORS[class]
 
+    local test = UnitHealth("player")
+
     PlayerContainer.frame_texture:Show()
-    PlayerContainer.frame_texture:SetTexture("Interface\\AddOns\\BetterUnitFrame\\Media\\No-Portrait-2-Rows-Shadow")
+    PlayerContainer.frame_texture:SetTexture("Interface\\AddOns\\BetterUnitFrame\\Media\\No-Portrait-2-Row-Shadow")
+    PlayerContainer.status_texture:SetTexture("Interface\\AddOns\\BetterUnitFrame\\Media\\No-Portrait-3-Row-Status")
+    PlayerContainer.frame_flash:SetTexture("Interface\\AddOns\\BetterUnitFrame\\Media\\No-Portrait-Frame-Flash")
 
     PlayerBars.health_bar:Show()
-    PlayerBars.health_bar:SetStatusBarTexture("UI-HUD-UnitFrame-Player-PortraitOff-Bar-Health")
-    PlayerBars.health_bar:ClearAllPoints()
-    PlayerBars.health_bar:SetPoint("TOPLEFT", PlayerFrame, "TOPLEFT", 21, -25)
-    PlayerBars.health_bar:SetWidth(190)
-    PlayerBars.health_bar:SetHeight(28)
+    PlayerBars.health_bar:SetStatusBarTexture("Interface\\AddOns\\BetterUnitFrame\\Media\\No-Portrait-Health-Bar")
+    
+    if test then PlayerBars.health_bar_text:SetText(tostring(test)) end
 
-    PlayerBars.health_bar_text:ClearAllPoints()
-    PlayerBars.health_bar_text:SetPoint("CENTER", PlayerBars.health_bar, "CENTER")
-    PlayerBars.health_bar_text:SetScale(1.5)
+    local dirtyCheck = false
+    hooksecurefunc(PlayerBars.health_bar_text, "SetText", function(self)
+        local health = UnitHealth("player")
+        if dirtyCheck then
+            dirtyCheck = false
+        else
+            dirtyCheck = true
+            if health then
+                self:SetText(tostring(health))
+            end
+        end
+    end)
+
     PlayerBars.health_bar_text:Show()
 
     PlayerBars.mana_bar:Show()
-    PlayerBars.mana_bar:SetStatusBarTexture("UI-HUD-UnitFrame-Player-PortraitOff-Bar-RunicPower")
-    PlayerBars.mana_bar:ClearAllPoints()
-    PlayerBars.mana_bar:SetPoint("TOPLEFT", PlayerFrame, "TOPLEFT", 22, -54)
-    PlayerBars.mana_bar:SetWidth(188)
-    PlayerBars.mana_bar:SetHeight(13)
-
-    if ResourceFrames.DEATHKNIGHT then
-        ResourceFrames.DEATHKNIGHT:ClearAllPoints()
-        ResourceFrames.DEATHKNIGHT:SetPoint("CENTER", PlayerFrame, "BOTTOM", 0, 0)
-    end
 
     PlayerContainer.player_name:Show()
-    PlayerContainer.player_name:ClearAllPoints()
-    PlayerContainer.player_name:SetPoint("TOPLEFT", PlayerFrame, "TOPLEFT", 35, -14)
-
 
     PlayerContainer.player_level:Show()
-    PlayerContainer.player_level:ClearAllPoints()
-    PlayerContainer.player_level:SetPoint("TOPRIGHT", PlayerFrame, "TOPRIGHT", -30, -14)
+
+    PlayerContext.rest_loop:Show()
+    PlayerContext.rest_loop:ClearAllPoints()
+    PlayerContext.rest_loop:SetPoint("TOPRIGHT", PlayerFrame, "TOPRIGHT", 0, -20)
+
+    -- PlayerContext.attack_icon:Show()
+    PlayerContext.attack_icon:ClearAllPoints()
+    PlayerContext.attack_icon:SetPoint("TOPRIGHT", PlayerFrame, "TOPRIGHT", -15, -35)
+
 end
