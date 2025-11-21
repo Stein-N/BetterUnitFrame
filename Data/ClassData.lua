7c73674e7ea53ae5419ec29520dc5e07a40d5e54
Stein-N <nico-stein-98@sbvl.de>
2025-11-21 19:30:36 +0100

    Adding Class Data, for now only DK for testing

---
+++ Data/ClassData.lua
@@ -0,0 +1,27 @@
+ClassData = {
+    -- Deathknight
+    Frost = {
+        frame_texture = "no_portrait_2_row",
+        frame_texture_shadow = "no_portrait_2_row_shadow",
+        status_texture = "no_portrait_2_row_status",
+        status_texture_shadow = "no_portrait_2_row_status_shadow",
+        power_type = "RUNIC_POWER",
+        resource_frame = RuneFrame
+    },
+    Blood = {
+        frame_texture = "no_portrait_2_row",
+        frame_texture_shadow = "no_portrait_2_row_shadow",
+        status_texture = "no_portrait_2_row_status",
+        status_texture_shadow = "no_portrait_2_row_status_shadow",
+        power_type = "RUNIC_POWER",
+        resource_frame = RuneFrame
+    },
+    Unholy = {
+        frame_texture = "no_portrait_2_row",
+        frame_texture_shadow = "no_portrait_2_row_shadow",
+        status_texture = "no_portrait_2_row_status",
+        status_texture_shadow = "no_portrait_2_row_status_shadow",
+        power_type = "RUNIC_POWER",
+        resource_frame = RuneFrame
+    },
+}
