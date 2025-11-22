2ea906ce2e3ca197815a139c6a7149b2d9f0d8f8
Stein-N <nico-stein-98@sbvl.de>
2025-11-22 14:41:49 +0100

    move BufSettings into BufData class

--- Data/BufData.lua
+++ Data/BufData.lua
@@ -1,11 +1,15 @@
 BufData = {
-    ["addonName"] = "BetterUnitFrame",
-    ["optionCategories"] = {
-        ["deDE"] = {
-            ["template"] = "Beispielkategorie"
-        },
-        ["enEN"] = {
-            ["template"] = "Example Category"
+    addon_name = "BetterUnitFrame"
+}
+
+BufSettings = {
+    toggle_player_frame = {
+        key = "Buf_Toggle_Player_Frame",
+        default = true,
+        enEn = {
+            name = "Apply Enhancements",
+            tooltip = "Apply all Enhancements to the Player Unit Frame",
+            button = "Show Preview"
         }
     }
 }
