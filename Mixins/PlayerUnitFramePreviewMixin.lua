bdc40294d0518f19c234970c05447bc94fc58d8d
Stein-N <nico-stein-98@sbvl.de>
2025-11-24 22:16:42 +0100

    adding base PlayerUnitFramePreviewMixin

---
+++ Mixins/PlayerUnitFramePreviewMixin.lua
@@ -0,0 +1,12 @@
+PlayerUnitFramePreviewMixin = {}
+PM = PlayerUnitFramePreviewMixin
+
+function PM:Initialize()end
+
+function PM:UpdatePreview()end
+
+function PM:LoadSettings()end
+
+function PM:ShowSettings()end
+
+function PM:HideSettings()end
