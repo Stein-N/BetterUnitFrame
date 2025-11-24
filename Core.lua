abcdbf1f04ecb5ab3f40c5e1f97b621bf6896dfb
Stein-N <nico-stein-98@sbvl.de>
2025-11-24 22:08:03 +0100

    adding Move Function to BufUtils

--- Core.lua
+++ Core.lua
@@ -13,7 +13,6 @@ frame:RegisterEvent("ADDON_LOADED")
 frame:RegisterEvent("PLAYER_ENTERING_WORLD")
 frame:RegisterEvent("PLAYER_SPECIALIZATION_CHANGED")
 
-
 frame:SetScript("OnEvent", function(self, event, ...)
     BufEvents:OnEvent(event, ...)
 end)
