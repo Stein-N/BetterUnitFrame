4894f9c0edd66dd1bcff4f95a6b48571aade25a0
Nico.Stein <nico.stein@vit-bund.de>
2025-11-25 11:53:14 +0100

    something stupid

--- Mixins/PlayerUnitFramePreviewMixin.lua
+++ Mixins/PlayerUnitFramePreviewMixin.lua
@@ -1,1 +1,10 @@
 PlayerUnitFramePreviewMixin = {}
+PUM = PlayerUnitFramePreviewMixin
+
+function PUM:OnShow()
+    PreviewModuleMixin:Init(self)
+end
+
+function PUM:OnHide()
+
+end
--- Mixins/PreviewModuleMixin.lua
+++ Mixins/PreviewModuleMixin.lua
@@ -3,12 +3,9 @@ PM = PreviewModuleMixin
 
 local build = false
 
-local f
-
-function PM:Init(parent, name)
+function PM:Init(parent)
     if not build then
-        f = CreateFrame("Frame", "BetterUnitFramePreviewModule"..name, parent, "PreviewModuleTemplate")
-        local tex = f:CreateTexture(nil, "BACKGROUND")
+        local tex = self:CreateTexture(nil, "BACKGROUND")
         tex:SetTexture("Interface\\AddOns\\BetterUnitFrame\\Media\\No-Portrait-Atlas")
         tex:SetTexCoord(BufMedia.GetTexCoords("no_portrait_2_row"))
         tex:SetAllPoints(self)
