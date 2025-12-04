18666227563a5bbe82ec900225913e43b9bada1b
Stein-N <nico-stein-98@sbvl.de>
2025-12-04 22:34:48 +0100

    move PlayerFrameComponent for FrameComponent

---
+++ Components/FrameComponents.lua
@@ -0,0 +1,11 @@
+PlayerFrameComponentMixin = CreateFromMixins(BaseComponentMixin)
+
+function PlayerFrameComponentMixin:BuildSettings(category)
+    if not self.isBuilt then
+        local init = Settings.CreatePanelInitializer("PlayerFrameComponentSettings", { component = self})
+        init:AddShownPredicate(function() return self.isFocused end)
+        SettingsPanel:GetLayout(category):AddInitializer(init)
+
+        self.isBuilt = true
+    end
+end
