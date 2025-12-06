ca61a0fc87ea1f391b62fade527ae6ac48c2d12a
Stein-N <nico-stein-98@sbvl.de>
2025-12-06 23:45:32 +0100

    Rebuild Settings Components

--- Settings/SettingComponents.lua
+++ Settings/SettingComponents.lua
@@ -1,114 +1,94 @@
 SettingsComponents = {}
 
-function SettingsComponents:CreateSlider(parent, label, min, max, formatter, updateFunc)
-    local holder = CreateFrame("Frame", nil, parent)
-    holder:SetHeight(40)
+local function CreateBaseFrame(parent)
+    local f = CreateFrame("Frame", nil, parent)
+    f:SetHeight(36)
 
-    holder.Label = holder:CreateFontString(nil, "OVERLAY", "GameFontNormal")
-    holder.Label:SetJustifyH("LEFT")
-    holder.Label:SetPoint("LEFT", 20, 0)
-    holder.Label:SetPoint("RIGHT", holder, "CENTER", -50, 0)
-    holder.Label:SetText(label)
+    f.Label = f:CreateFontString("Label", "OVERLAY", "GameFontNormal")
+    f.Label:SetJustifyH("LEFT")
+    f.Label:SetPoint("LEFT", 20, 0)
+    f.Label:SetPoint("RIGHT", f, "CENTER", -50, 0)
 
-    holder.Slider = CreateFrame("Slider", nil, holder, "MinimalSliderWithSteppersTemplate")
-    holder.Slider:SetPoint("LEFT", holder, "CENTER", -32, 0)
-    holder.Slider:SetPoint("RIGHT", -45, 0)
-    holder.Slider:SetHeight(20)
-    holder.Slider:Init(max, min, max, max - min, {
-        [MinimalSliderWithSteppersMixin.Label.Right]  = CreateMinimalSliderFormatter(MinimalSliderWithSteppersMixin.Label.Right, function(value)
-        return WHITE_FONT_COLOR:WrapTextInColorCode(formatter(value))
-        end)
+    f.HoverBackground = f:CreateTexture("HoverBackground", "BACKGROUND")
+    f.HoverBackground:SetPoint("TOPLEFT", f, "TOPLEFT", -10, 0)
+    f.HoverBackground:SetPoint("BOTTOMRIGHT", f, "BOTTOMRIGHT", -5, 0)
+    f.HoverBackground:SetColorTexture(1, 1, 1, 0.1)
+    f.HoverBackground:Hide()
+
+    function f:SetLabel(label)
+        self.Label:SetText(label)
+    end
+
+    function f:OnEnter()
+        self.HoverBackground:Show()
+    end
+
+    function f:OnLeave()
+        self.HoverBackground:Hide()
+    end
+
+    f:SetScript("OnEnter", f.OnEnter)
+    f:SetScript("OnLeave", f.OnLeave)
+
+    return f
+end
+
+function SettingsComponents:CreateSlider(parent, label, min, max, step, suffix, updateFunc)
+    local f = CreateBaseFrame(parent)
+    f:SetLabel(label)
+
+    f.Slider = CreateFrame("Slider", nil, f, "MinimalSliderWithSteppersTemplate")
+    f.Slider:SetPoint("LEFT", f, "CENTER", -53, 0)
+    f.Slider:SetWidth(260)
+
+    f.Slider.Slider:SetScript("OnEnter", function() f:OnEnter() end)
+    f.Slider.Slider:SetScript("OnLeave", function() f:OnLeave() end)
+
+    f.Slider:Init(max, min, max, (max - min) / step, {
+        [MinimalSliderWithSteppersMixin.Label.Right] = CreateMinimalSliderFormatter(MinimalSliderWithSteppersMixin.Label.Right,
+            function(v) return v .. (suffix or "") end)
     })
 
-    holder.Slider:RegisterCallback(MinimalSliderWithSteppersMixin.Event.OnValueChanged, function(_, value)
-        updateFunc(value)
-    end)
+    f.Slider:RegisterCallback(MinimalSliderWithSteppersMixin.Event.OnValueChanged, function(_, v) updateFunc(v) end)
 
-    function holder:GetValue()
-        return holder.Slider.Slider:GetValue()
-    end
-
-    function holder:SetValue(value)
-        return holder.Slider:SetValue(value)
-    end
-
-    holder:SetScript("OnMouseWheel", function(_, delta)
-        if holder.Slider.Slider:IsEnabled() then
-        holder.Slider:SetValue(holder.Slider.Slider:GetValue() + delta)
-        end
-    end)
-
-    table.insert(parent.settings, holder)
+    table.insert(parent.layout, f)
 end
 
 function SettingsComponents:CreateCheckbox(parent, label, updateFunc)
-    local holder = CreateFrame("Frame", nil, parent)
-    holder:SetHeight(40)
+    local f = CreateBaseFrame(parent)
+    f:SetLabel(label)
 
-    holder.Label = holder:CreateFontString(nil, "OVERLAY", "GameFontNormal")
-    holder.Label:SetJustifyH("LEFT")
-    holder.Label:SetPoint("LEFT", 20, 0)
-    holder.Label:SetPoint("RIGHT", holder, "CENTER", -50, 0)
-    holder.Label:SetText(label)
+    f.Checkbox = CreateFrame("CheckButton", nil, f, "SettingsCheckboxTemplate")
+    f.Checkbox:SetPoint("LEFT", f, "CENTER", -53, 0)
 
-    holder.Checkbox = CreateFrame("CheckButton", nil, holder, "SettingsCheckboxTemplate")
-    holder.Checkbox:SetPoint("LEFT", holder, "CENTER", -15, 0)
+    f.Checkbox:SetScript("OnClick", function() updateFunc(f.Checkbox:GetChecked()) end)
 
 
-    function holder:SetValue(value)
-        holder.Checkbox:SetChecked(value)
-    end
-
-    holder:SetScript("OnEnter", function()
-        holder.Checkbox:OnEnter()
-    end)
-
-    holder:SetScript("OnLeave", function()
-        holder.Checkbox:OnLeave()
-    end)
-
-    holder:SetScript("OnMouseUp", function()
-        holder.Checkbox:Click()
-    end)
-
-    holder.Checkbox:SetScript("OnClick", function()
-        updateFunc(holder.Checkbox:GetChecked())
-    end)
-
-    table.insert(parent.settings, holder)
+    table.insert(parent.layout, f)
 end
 
---TODO: needs changes
-function SettingsComponents:CreateDropdown(parent, labelText, isSelectedCallback, onSelectionCallback)
-    local frame = CreateFrame("Frame", nil, parent)
-    local dropdown = CreateFrame("DropdownButton", nil, frame, "WowStyle1DropdownTemplate")
-    dropdown:SetWidth(250)
-    dropdown:SetPoint("LEFT", frame, "CENTER", -32, 0)
+function SettingsComponents:CreateDropdown(parent, label, items, onSelect)
+    local f = CreateBaseFrame(parent)
+    f:SetLabel(label)
 
-    local label = frame:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
-    label:SetPoint("LEFT", 20, 0)
-    label:SetPoint("RIGHT", frame, "CENTER", -50, 0)
-    label:SetJustifyH("RIGHT")
-    label:SetText(labelText)
-    frame:SetPoint("LEFT", 30, 0)
-    frame:SetPoint("RIGHT", -30, 0)
+    f.Dropdown = CreateFrame("DropdownButton", nil, f, "WowStyle1DropdownTemplate")
+    f.Dropdown:SetPoint("LEFT", f, "CENTER", -50, 0)
+    f.Dropdown:SetWidth(250)
 
-    frame.Init = function(_, entryLabels, values)
-        local entries = {}
-        for index = 1, #entryLabels do
-            table.insert(entries, {entryLabels[index], values[index]})
+    f.Dropdown:SetScript("OnEnter", function() f:OnEnter() end)
+    f.Dropdown:SetScript("OnLeave", function() f:OnLeave() end)
+
+    f.Dropdown:SetupMenu(function(dropdown, root)
+        for _, item in ipairs(items) do
+            root:CreateButton(item.label, function()
+                dropdown:OverrideText(item.label)
+
+                if onSelect then
+                    onSelect(item.value)
+                end
+            end)
         end
-        
-        MenuUtil.CreateRadioMenu(dropdown, isSelectedCallback, onSelectionCallback, unpack(entries))
-    end
+    end)
 
-    frame.SetValue = function(_, _)
-        dropdown:GenerateMenu()
-    end
-
-    frame.Label = label
-    frame.DropDown = dropdown
-    frame:SetHeight(40)
-
-    return frame
+    table.insert(parent.layout, f)
 end
